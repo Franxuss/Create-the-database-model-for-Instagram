404ef1f5e72b754e0fef6e8c903eeb0aa746a1f8
Tibisfly <vielmatorotibisay@gmail.com>
2020-11-16 19:18:26 +0000

    Testing Alchemy

--- src/models.py
+++ src/models.py
@@ -8,23 +8,50 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    first_name = Column(String)
+    last_name = Column(String)
+    address = Column(String)
+    email = Column(String)
+    password = Column(String)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('user.id'))
+#     person = relationship(User)
+
+class Likes(Base):
+    __tablename__ = 'likes'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user = Column(String)
+    date = Column(String)
+  
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    image = Column(String)
+    date = Column(String)
+    user = Column(String)
+
+class DirectMessage(Base):
+    __tablename__ = 'directMessage'
+    id = Column(Integer, primary_key=True)
+    user = Column(String)
+    date = Column(String)
+    voicenote = Column(String)
+    message = Column(String(300))
+    image = Column(String)
 
     def to_dict(self):
         return {}
